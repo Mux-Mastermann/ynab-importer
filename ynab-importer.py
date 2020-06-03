b92d0742153c10709c1698e37d3fc7bd3640e042
Mux-Mastermann <43514294+Mux-Mastermann@users.noreply.github.com>
2020-06-03 22:14:52 +0200

    Multiple Transaction post

--- ynab-importer.py
+++ ynab-importer.py
@@ -1,32 +1,75 @@
+import csv
+import json
+
 import requests
 
-from config import Token
+from config import budgets, token
 
-# set authorization header
-headers = {"Authorization": f"Bearer {Token}"}
 
-# get all budgets
-response = requests.get(
-    "https://api.youneedabudget.com/v1/budgets", headers=headers)
+# set auth header
+headers = {"Authorization": f"Bearer {token}"}
 
-# get first budget id 
-response_data = response.json()
-budget_id = response_data["data"]["budgets"][0]["id"]
 
-# set data for new transaction posting
-data = {"transaction": {
-    "account_id": "",
-    "date": "2020-06-01",
-    "amount": -5000,
-    "payee_name": "",
-    "memo": "",
-    # Format: [YNAB]:Amount:Date:[Ocurrences of same amount and date]
-    "import_id": "YNAB:5000:2020-06-02:1"}
-    }
+def main():
+    # TODO hardcoded budget id
+    budget_id = "6a71e880-3f3c-421e-a6fb-db31f08553ca"
+    post_transactions(budget_id, transform_transactions("todo"))
 
-# post new transaction(s)
-r = requests.post(
-    f"https://api.youneedabudget.com/v1/budgets/{budget_id}/transactions", headers=headers, json=data)
 
-# print response
-print(r.text)
+def transform_transactions(account):
+    """Create an array of transactions from input csv"""
+    # create the array to use for the final post request
+    transactions = []
+
+    # just for now hardcoded
+    # TODO dynamic account selection from json / or request for accounts
+    account = "6bfb4dab-b461-4fc8-b964-a4ffa0dc5da3"
+
+    with open("import/ynab.csv", "r") as csv_file:
+        csv_reader = csv.DictReader(csv_file, delimiter=";")
+
+        for line in csv_reader:
+            # create a transaction for each line
+            transaction = {
+                "account_id":   account, # string
+                "date":         line['date'], # string [YYYY-MM-DD]
+                "amount":       line['inflow'], # int, [Amount x 1000]
+                "payee_name":   " ".join(line['payee'].split()), # string
+                "memo":         " ".join(line['memo'].split()), # string
+                "import_id":  f"YNAB:{line['inflow']}:{line['date']}:1" # Format: [YNAB]:Amount:Date:[Ocurrences of same amount and date]
+            }
+            # append this transaction to final array of transactions
+            transactions.append(transaction)
+    return transactions
+
+
+def get_all_accounts(budget_id, token):
+    """get all accounts from given budget"""
+    
+    # get all accounts
+    response = requests.get(
+        f"https://api.youneedabudget.com/v1/budgets/{budget_id}/accounts", headers=headers).json()
+    # create array for the accounts
+    accounts = []
+    for account in response["data"]["accounts"]:
+        accounts.append({"id": account["id"], "name": account["name"]})
+    # write to json file
+    with open("accounts.json", "w") as wf:
+        json.dump(accounts, wf, indent=4)
+
+
+def post_transactions(budget_id, transactions):
+
+    # set data for new transaction posting
+    data = {"transactions": transactions}
+
+    # post new transaction(s)
+    post_response = requests.post(
+        f"https://api.youneedabudget.com/v1/budgets/{budget_id}/transactions", headers=headers, json=data)
+
+    # print response
+    print(post_response)
+
+
+if __name__ == "__main__":
+    main()
